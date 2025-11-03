31c42b16e48df54cd1b0ddb7ac9d504d72d39a33
funnyboiboi88 <ling.davidn@gmail.com>
2025-11-04 00:30:02 +0800

    Update hangman.py
    created start button

--- hangman.py
+++ hangman.py
@@ -1,28 +1,23 @@
 import pygame
 import pygame_gui
 import functions
+import functions
 
+# initialise GUI window
 pygame.init()
 screen = pygame.display.set_mode((1000,1000))
 clock = pygame.time.Clock()
 running =  True
+game_started = False
+screen.fill((255,255,255)) 
+pygame.display.set_caption('Hangman Game')
+manager = pygame_gui.UIManager((1000, 1000))
 
 # initialise font and word
 font = pygame.font.Font(None, 48)  # None = default font, 48 = size
 word = "HANG MAN GAME"
 
-#initialise GUI
-pygame.display.set_caption('Hangman Game')
-manager = pygame_gui.UIManager((1000, 1000))
-
-screen.fill((255,255,255)) # add logic instead here 
-
-hello_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((150, 800), (150, 50)),text='Gay-meh Starto',manager=manager)
-
-
-#add functions 
-
-
+start_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((150, 800), (150, 50)),text='Gay-meh Starto',manager=manager)
 
 
 while running:
@@ -34,20 +29,21 @@ while running:
             running = False
         
         if event.type == pygame_gui.UI_BUTTON_PRESSED:
-            if event.ui_element == hello_button:
-                print("Hello World!")
-                pygame.draw.rect(screen, (0,0,0), (100, 100, 800, 600), 10)
-                text_surf = font.render(word, True, (0,0,0))
-                text_rect = text_surf.get_rect(center=(500, 80))
-                screen.blit(text_surf, text_rect)
-                functions.drawHangman(screen, 3)  
+            
+            if event.ui_element == start_button:
+                print("Game has started!")
+                game_started = True
+                start_button.hide()
                 
     manager.update(time_delta)   
+    screen.fill((255,255,255))  #frame clearer
 
-    
-    # screen.fill((255, 255, 255))
+    if game_started == True:
+        pygame.draw.rect(screen, (0,0,0), (100, 100, 800, 600), 10)
+        text_surf = font.render(word, True, (0,0,0))
+        text_rect = text_surf.get_rect(center=(500, 80))
+        screen.blit(text_surf, text_rect)
 
-
-    manager.draw_ui(screen)                              # draw GUI elements (buttons, etc.)
+    manager.draw_ui(screen)        # draw GUI elements (buttons, etc.)
 
     pygame.display.update()
