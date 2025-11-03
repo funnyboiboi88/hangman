e465b52fc97ec1ce247453e70e51f2ad25184619
funnyboiboi88 <ling.davidn@gmail.com>
2025-11-04 00:09:48 +0800

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,17 +1,22 @@
 import pygame
+import pygame_gui
 
 pygame.init()
-screen = pygame.display.set_mode((1280,720))
+screen = pygame.display.set_mode((1000,1000))
 clock = pygame.time.Clock()
 running =  True
 
 # initialise font and word
 font = pygame.font.Font(None, 48)  # None = default font, 48 = size
-word = "HELLO PYGAME"
+word = "HANG MAN GAME"
 
 #initialise GUI
+pygame.display.set_caption('Hangman Game')
+manager = pygame_gui.UIManager((1000, 1000))
+
 screen.fill((255,255,255)) # add logic instead here 
-pygame.draw.rect(screen, (0,0,0), (300,360,200,100),10) # example rectangle
+
+hello_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((150, 800), (150, 50)),text='Gay-meh Starto',manager=manager)
 
 
 #add functions 
@@ -22,16 +27,27 @@ pygame.draw.rect(screen, (0,0,0), (300,360,200,100),10) # example rectangle
 
 
 while running:
+    time_delta = clock.tick(60) / 1000.0 # seconds since last frame
+    
     for event in pygame.event.get():
+        manager.process_events(event)  
         if event.type == pygame.QUIT:
             running = False
+        
+        if event.type == pygame_gui.UI_BUTTON_PRESSED:
+            if event.ui_element == hello_button:
+                print("Hello World!")
+                pygame.draw.rect(screen, (0,0,0), (100, 100, 800, 600), 10)
+                text_surf = font.render(word, True, (0,0,0))
+                text_rect = text_surf.get_rect(center=(500, 80))
+                screen.blit(text_surf, text_rect)
+                
+    manager.update(time_delta)   
 
-    screen.fill((255,255,255)) # add logic instead here 
-    pygame.draw.rect(screen, (0,0,0), (300,360,200,100),10) # example rectangle
     
-    text_surf = font.render(word, True, (0,0,0))   # (text, antialias, color)
-    text_rect = text_surf.get_rect(center=(640, 80))  # position: center x=640, y=80
-    screen.blit(text_surf, text_rect)
-    
-    pygame.display.flip()   #refresh on-screen display
-    clock.tick(60)          # next frame 60 FPS    
+    # screen.fill((255, 255, 255))
+
+
+    manager.draw_ui(screen)                              # draw GUI elements (buttons, etc.)
+
+    pygame.display.update()
